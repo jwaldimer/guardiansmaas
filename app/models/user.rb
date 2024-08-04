97f55e97aa2d55643c5ce8c7f95eb4ff8dd9cff1
jwaldimer <jwaldimer@gmail.com>
2024-08-04 00:35:23 -0500

    finish seed for shifts and set color for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
+  before_create :generate_unique_color
     
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
@@ -7,4 +8,13 @@ class User < ApplicationRecord
 
   has_many :shifts
   has_many :availabilities
+
+  private
+
+  def generate_unique_color
+    self.color = loop do
+      color = SecureRandom.hex(3)
+      break random_color unless User.exists?(color: color)
+    end
+  end
 end
--- db/seeds/shifts_seed.rb
+++ db/seeds/shifts_seed.rb
@@ -8,13 +8,14 @@ Service.all.each_with_index do |service, index|
   weeks = Week.where(number: (current_week..52).to_a)
 
   weeks.each do |week|
-    # Set week shifts
+    # Set shifts based on contracts
     (1..7).each do |day|
       contract_hours = day <= 5 ? weekdays : weekend
-      date = Date.commercial(week.year, week.number, day)
-      date_formated = I18n.l(date, format: '%A %d de %B', locale: :es).capitalize
 
       contract_hours.each do |hour|
+        date = Date.commercial(week.year, week.number, day)
+        date_formated = I18n.l(date, format: '%A %d de %B', locale: :es).capitalize
+      
         Shift.create(
           service: service,
           week: week,
